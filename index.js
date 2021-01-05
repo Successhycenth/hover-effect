7b0f1b0f5c96428a67e0d98fea634fbad33007d5
Successhycenth <successhycenth8@gmail.com>
2021-01-05 09:19:06 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+new hoverEffect({
+  parent: document.querySelector('.distortion'),
+  intensity: 0.2,
+  image1: './pexels-maria.jpg',
+  image2: './pexels-vitor2.jpg',
+  speedIn: 2,
+  speedOut: 5,
+
+  displacementImage: './color.png'
+});
